f318ff2e9052cb0e22589d1957f29116c4882224
EmmaKibore <emmakibore@gmail.com>
2019-03-01 16:12:13 +0300

    added start button to html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-var dieImages = 
+var diceImages = [dice1:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
+                  dice2:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
+                  dice3:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
+                  dice4:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
+                  dice5:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
+                  dice6:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice]
+
+var firstPlayer = "";
+var secondPlayer = "";
+
+var throwdice = function () {
+  return Math.floor(6 * Math.random())+1;
+}
+
+function player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
