ed6b2d6baf3b7470b59253b8dba0e0b1c51b1ff3
EmmaKibore <emmakibore@gmail.com>
2019-03-03 22:38:21 +0300

    add initial changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//business logic
 var diceImages = [dice1:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
                   dice2:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
                   dice3:https/home/emmah/Desktop/moringa-school-projects/Pig-Dice
@@ -7,10 +8,11 @@ var diceImages = [dice1:https/home/emmah/Desktop/moringa-school-projects/Pig-Dic
 
 var firstPlayer = "";
 var secondPlayer = "";
-
+var dice = [1,2,3,4,5,6]
 var throwdice = function () {
-  return Math.floor(6 * Math.random())+1;
+  return Math.floor(Math.random *6());
 }
+
 
 function player(turn) {
   this.roll = 0;
@@ -18,4 +20,28 @@ function player(turn) {
   this.totalscore = 0;
   this.turn = turn;
   this.playerName;
+}
+//checking for firstPlayer
+player.prototype.rollone = function() {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("sorry " + this.firstPlayerName + ",you rolled a 1! your turn is over sucker!")
+
+  } else {
+    this.tempscore += this.roll;
+  }
+}
+// Hold
+player.prototype.hold =function () {
+  this.totalscore += this.tempscore
+  this.tempscore = 0;
+  //this.changeturn();
+  alert(this.firstPlayerName + ",your turn is over,next player!");
+  }
+}
+//check for 100
+firstPlayer.prototype.winnerCheck = function () {
+  if (this.totalscore >= 100) {
+    alert (this.firstPlayerName + "you are the winner!");
+  }
 }
